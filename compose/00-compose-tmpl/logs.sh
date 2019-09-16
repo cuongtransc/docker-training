53e459262caec8df73c306bc139ae72c915730b7
Cuong Tran <cuongtransc@gmail.com>
2019-09-16 20:47:34 +0700

    Update compose template: restart=no

---
+++ compose/00-compose-tmpl/logs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+CONTAINER_NAME=app_app_1
+
+COLUMNS=129
+LINES=35
+
+# check container exists
+docker ps -a | awk '{print $NF}' | grep ${CONTAINER_NAME} &>/dev/null
+
+if [[ $? != 0 ]]; then
+    echo "You need to create docker container first"
+    echo "Using: docker-compose up -d"
+else
+    docker logs -f --tail 30 ${CONTAINER_NAME}
+fi
